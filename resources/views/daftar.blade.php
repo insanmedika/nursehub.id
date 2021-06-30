af17cf522ed068435f3c224a622865011f61b3ce
ramdan <ramdanandchandra@gmail.com>
2021-06-30 15:12:21 +0700

    nursehub id
    update

--- resources/views/daftar.blade.php
+++ resources/views/daftar.blade.php
@@ -318,53 +318,53 @@
                 </p>
             </div>
         </section>
-        <section class="relative mx-auto py-1 bg-white shadow sticky bottom-0 z-50">
-            <div class="px-7 bg-white shadow-lg rounded-2xl sm:hidden">
-                <div class="flex">
-                    <div class="flex-1 group">
-                        <a href="#" class="flex items-end justify-center text-center mx-auto px-4 pt-2 w-full text-gray-400 group-hover:text-primary border-b-2 border-transparent group-hover:border-primary">
-                            <span class="block px-1 pt-1 pb-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                                  </svg>
-                                <span class="block text-xs pb-1">Home</span>
-                            </span>
-                        </a>
-                    </div>
-                   
-                    <div class="flex-1 group">
-                        <a href="#" class="flex items-end justify-center text-center mx-auto px-4 pt-2 w-full text-gray-400 group-hover:text-primary border-b-2 border-transparent group-hover:border-primary">
-                            <span class="block px-1 pt-1 pb-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                                  </svg>
-                                <span class="block text-xs pb-1">Search</span>
-                            </span>
-                        </a>
-                    </div>
-                    <div class="flex-1 group">
-                        <a href="#" class="flex items-end justify-center text-center mx-auto px-4 pt-2 w-full text-gray-400 group-hover:text-primary border-b-2 border-transparent group-hover:border-primary">
-                            <span class="block px-1 pt-1 pb-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-                                  </svg>
-                                <span class="block text-xs pb-1">Blog</span>
-                            </span>
-                        </a>
-                    </div>
-                    <div class="flex-1 group">
-                        <a href="#" class="flex items-end justify-center text-center mx-auto px-4 pt-2 w-full text-gray-400 group-hover:text-primary border-b-2 border-transparent group-hover:border-primary">
-                            <span class="block px-1 pt-1 pb-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-                                  </svg>
-                                <span class="block text-xs pb-1">Chat</span>
-                            </span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            </section>
+        <section class="relative mx-auto py-2 px-4  shadow-2 sticky bottom-1 z-50">
+          <div class="px-7 bg-white shadow-lg rounded-2xl sm:hidden">
+              <div class="flex">
+                  <div class="flex-1 group">
+                      <a href="/" class="flex items-end justify-center text-center mx-auto px-4 pt-2 w-full text-gray-400 group-hover:text-primary border-b-2 border-transparent group-hover:border-primary">
+                          <span class="block px-1 pt-1 pb-2">
+                              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                                </svg>
+                              <span class="block text-xs pb-1">Home</span>
+                          </span>
+                      </a>
+                  </div>
+                 
+                  <div class="flex-1 group">
+                      <a href="#" class="flex items-end justify-center text-center mx-auto px-4 pt-2 w-full text-gray-400 group-hover:text-primary border-b-2 border-transparent group-hover:border-primary">
+                          <span class="block px-1 pt-1 pb-2">
+                              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                                </svg>
+                              <span class="block text-xs pb-1">Search</span>
+                          </span>
+                      </a>
+                  </div>
+                  <div class="flex-1 group">
+                      <a href="/blog" class="flex items-end justify-center text-center mx-auto px-4 pt-2 w-full text-gray-400 group-hover:text-primary border-b-2 border-transparent group-hover:border-primary">
+                          <span class="block px-1 pt-1 pb-2">
+                              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                                </svg>
+                              <span class="block text-xs pb-1">Blog</span>
+                          </span>
+                      </a>
+                  </div>
+                  <div class="flex-1 group">
+                      <a href="https://wa.me/6281336696699" class="flex items-end justify-center text-center mx-auto px-4 pt-2 w-full text-gray-400 group-hover:text-primary border-b-2 border-transparent group-hover:border-primary">
+                          <span class="block px-1 pt-1 pb-2">
+                              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                                </svg>
+                              <span class="block text-xs pb-1">Chat</span>
+                          </span>
+                      </a>
+                  </div>
+              </div>
+          </div>
+          </section>
             <script>
               function closeAlert(event){
                 let element = event.target;
@@ -374,5 +374,6 @@
                 element.parentNode.parentNode.removeChild(element.parentNode);
               }
             </script>
+            <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     </body>
 </html>
